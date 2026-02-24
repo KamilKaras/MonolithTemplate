65a22edb18396b25e3ee6da47eb0bcc7c20cd9c4
Kamil Karasiewicz <kkarasiewicz@vanto.pl>
2026-02-24 19:36:13 +0100

    AddReact

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "../components/pages/LoginPage/LoginPage";
+import "./App.scss";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/confirmation" element={<div></div>} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ apps/web/src/components/atoms/CustomBtn/types.ts
@@ -0,0 +1,4 @@
+export interface CustomBtnProps {
+  label: string;
+  onClick: () => void;
+}
