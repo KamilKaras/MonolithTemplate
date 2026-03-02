2ed86fca0785837372ada926d367757ae447c395
Kamil Karasiewicz <kkarasiewicz@vanto.pl>
2026-03-02 22:04:18 +0100

    add-theme

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.tsx";
+import "./app/theme/themes/mytheme/theme.scss";
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
