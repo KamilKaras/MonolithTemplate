ac6d05a1761c95240ea23622a9c2a2b14ef9baab
Kamil Karasiewicz <kkarasiewicz@vanto.pl>
2026-03-05 07:30:19 +0100

    docker

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://api:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
   css: {
     preprocessorOptions: {
