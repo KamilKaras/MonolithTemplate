e629a17c30de0db47b08f6330ce51f8d1d253fc5
Kamil Karasiewicz <kkarasiewicz@vanto.pl>
2026-07-12 09:37:42 +0200

    fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     host: true,
     port: 3000,
